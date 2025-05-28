f44ec68ebe5b1553b5f87486218decdcf086e395
Uallace Macedo <uallace.macedo@outlook.com>
2025-05-28 12:02:44 -0300

    feat: add newitems section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import NewItems from "./components/NewItems";
 
 function App() {
   return (
     <div className="w-full max-w-[1440px] mx-auto bg-white">
       <Hero />
       <Features />
+      <NewItems />
     </div>
   )
 }

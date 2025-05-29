31cb651833c48ea61917076ef724f9e0ca3a06ac
Uallace Macedo <uallace.macedo@outlook.com>
2025-05-29 12:56:33 -0300

    feat: add header + mobile header

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Products from "./components/Products";
 import Testimonials from "./components/Testimonials";
 import Newsletter from "./components/Newsletter";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="w-full max-w-[1440px] mx-auto bg-white">
+      <Header />
       <Hero />
       <Features />
       <NewItems />
